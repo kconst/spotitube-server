c57dc774c5751f0dab63c454ec2e1e9036ec512f
kconst <kconst@gmail.com>
2015-10-14 14:51:53 -0400

    updating for new callback url

--- routes/callback.js
+++ routes/callback.js
@@ -60,7 +60,7 @@ router.get('/', function(req, res) {
                 });*/
 
                 // we can also pass the token to the browser to make requests from there
-                res.redirect('http://localhost:63342/spotitube-ui/app#' +
+                res.redirect('http://localhost:9000/#/loginSpotify/' +
                     querystring.stringify({
                         access_token: access_token,
                         refresh_token: refresh_token
